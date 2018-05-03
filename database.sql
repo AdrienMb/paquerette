8326a3a5962b38ffc87fe6a413ac72d99185b19d
AdrienMb <adrien.mbirko@gmail.com>
2018-05-03 10:05:45 +0200

    third commit

--- database.sql
+++ database.sql
@@ -1,10 +1,4 @@
-IF db_id('paquerette') IS NULL 
-    CREATE DATABASE paquerette
-
-GO
-
 CREATE TABLE IF NOT EXISTS Job (
-job_id int NOT NULL AUTO INCREMENT,
-job_name char(20) NULL, 
-PRIMARY KEY (job_id) 
+job_id SERIAL PRIMARY KEY     NOT NULL,
+job_name char(20) NULL
 );
