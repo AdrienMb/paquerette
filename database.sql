bbd6c931056a85eed65e6374e90cecc0cf1d100a
WilliamDumont <WilliamDumont@192.168.1.80>
2018-05-06 18:16:55 +0200

    sql table parcours and job_parcours

--- database.sql
+++ database.sql
@@ -1,4 +1,15 @@
 CREATE TABLE IF NOT EXISTS job (
-job_id SERIAL PRIMARY KEY     NOT NULL,
-job_name char(20) NULL
+job_id SERIAL PRIMARY KEY NOT NULL,
+job_name char(200) NULL
+);
+
+CREATE TABLE IF NOT EXISTS parcours (
+parcours_id SERIAL PRIMARY KEY NOT NULL,
+parcours_name char(200) NULL,
+parcours_link_isep TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS job_parcours (
+job_id INT NOT NULL,
+parcours_id INT NOT NULL
 );
