4ce06de7f2d66d6a6e1099b675acfe0f05081aa1
WilliamDumont <WilliamDumont@192.168.1.80>
2018-05-07 20:47:36 +0200

    sql domaine

--- database.sql
+++ database.sql
@@ -9,12 +9,25 @@ parcours_name char(200) NULL,
 parcours_link_isep TEXT NULL
 );
 
+CREATE TABLE IF NOT EXISTS domaine (
+domaine_id SERIAL PRIMARY KEY NOT NULL,
+domaine_name char(200) NULL
+);
+
 CREATE TABLE IF NOT EXISTS job_parcours (
 job_id INT NOT NULL,
 parcours_id INT NOT NULL,
 PRIMARY KEY (job_id, parcours_id),
 CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES job (job_id),
 CONSTRAINT fk_parcours FOREIGN KEY (parcours_id) REFERENCES parcours (parcours_id)
+);
+
+CREATE TABLE IF NOT EXISTS job_domaine (
+job_id INT NOT NULL,
+domaine_id INT NOT NULL,
+PRIMARY KEY (job_id, domaine_id),
+CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES job (job_id),
+CONSTRAINT fk_domaine FOREIGN KEY (domaine_id) REFERENCES domaine (domaine_id)
 );
 
 INSERT INTO job (job_name) VALUES ('dev');
